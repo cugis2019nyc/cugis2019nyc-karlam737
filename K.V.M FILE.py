ba15cebe4ec4b1484b87af54edebed97a0a125fb
karlam737 <54039923+karlam737@users.noreply.github.com>
2019-08-13 13:41:46 -0400

    Add files via upload

---
+++ K.V.M FILE.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+de
+This is a temporary script file.
+"""
+print ("hello world")
+print ("how's life?")
+print("13")
+print("Karla")
+5*2
+5/2
+5-2
+5**2
+8/9*3
+10985*325
+9802*472
+def add(a,b):
+    add = (a+b)
+    print(add)
+    
+add(2,3)
+
+add(10985.345,9802.472)
+
+add (56,65)
+
+print("the sum of 1 and 3 is 4')
+
+def aqua(a,b,c):
+    aqua =a*b*C
+    print(aqua)
+
+aqua (5,5,5)
+print ("the product of",a,b"and",c "is" aqua)
+1/2*8*18
+print("the area of the triangle is 72.0 beacause the base is 8 and the height is 18")
+
+1/2*7*8
+print ("the area of a triangle is 28.0 because the base 7 and the height is 8"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
